d2c743b22770504d0d311f2cede513d7dddee5e7
Alexandre Spatola <alexandre.spatola@etu.umontpellier.fr>
2022-03-23 09:53:54 +0100

    Manhatan tier gameplay

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class GameDirector : MonoBehaviour
 {
-    public PieceSelector selectedPiece = null;
+    private PieceSelector selectedPiece = null;
+    private Game game = new Game();
+    int player = 0;
 
     public void Clicked(CaseSelector c)
     {
         if (!selectedPiece) return;
+        if (!game.PutPiece(c.x, c.y, selectedPiece.type)) return;
+
         MoveTo mt = selectedPiece.gameObject.AddComponent<MoveTo>();
         mt.duration = 1;
         mt.destination = c.transform.position + selectedPiece.offset;
-        selectedPiece.enabled = false;
         selectedPiece = null;
+        if (game.CheckWin()) Debug.Log("Player " + (player + 1) + " wins!");
     }
 
     public void Clicked(PieceSelector p)
     {
-        if (!selectedPiece) selectedPiece = p;
+        if (selectedPiece) return;
+
+        selectedPiece = p;
+        player = 1 - player;
+
+        MoveTo mt = p.gameObject.AddComponent<MoveTo>();
+        mt.duration = 0.1f;
+        mt.destination = p.transform.position + Vector3.up * 0.1f;
+        selectedPiece.enabled = false;
     }
 }
